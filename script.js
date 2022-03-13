65a2dcdc4b4877045496e1f1bf8f8c2b9a4d0262
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2022-03-13 21:13:51 +0500

    Commit

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal() {
   modal.classList.add('show-modal');
@@ -39,6 +41,24 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Fetches Bookmarks from Local Storage
+function fetchBookmarks() {
+  // Get bookmarks from local storage if available
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    // Create bookmarks array in local storage
+    bookmarks = [
+      {
+        name: 'Jacinto Design',
+        url: 'https://jacintodesign.com',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  console.log(bookmarks);
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
   e.preventDefault();
@@ -47,12 +67,22 @@ function storeBookmark(e) {
   if (!urlValue.includes('http://', 'https://')) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Event Listener
-
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
